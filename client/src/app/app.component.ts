49bbd54a8c6b80b9ead1b9daaeb938aed0f7f50c
Avitalys <avitalyo52@gmail.com>
2023-06-09 20:05:02 +0300

    Added http GET request to server

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,11 +1,26 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  
   title = 'Customers Management App';
   subTitle= 'Welcome!';
+  customers: any;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http.get('https://localhost:8080/api/customers').subscribe({
+      next: response => this.customers = response,
+      error: error => alert(error),
+      complete: () => console.log('GET Request Completed')
+    });
+  }
+
 }
--- webapi/Program.cs
+++ webapi/Program.cs
@@ -32,13 +32,15 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
         };
     });
 
+builder.Services.AddCors();
 builder.Services.AddAuthorization();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseCors(builder => 
+    builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
 
-// app.UseCors();
 app.UseAuthentication();
 app.UseAuthorization();
 
